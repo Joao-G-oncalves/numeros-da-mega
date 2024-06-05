e1886a58047ef11ca1ba57d7d1daaa84b016fadb
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-04 21:11:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# numeros-da-mega
+ Minha solução para criar os números da mega sena de forma aleatória
---
+++ main.py
@@ -0,0 +1,26 @@
+from random import randint
+
+def new_func():
+    x = int(input('Digite o número de jogos: '))
+    return x
+
+x = new_func()
+
+def criar_mega():
+    aleatorio = []
+    while len(aleatorio) < 6:
+        numero = randint(1, 60)
+        if numero not in aleatorio:
+            aleatorio.append(numero)
+            aleatorio.sort()
+    return aleatorio
+
+
+def quantidade_jogos(num):
+    for _ in range(num):
+        print(f'{'---'*8}\n{criar_mega()}')
+
+
+quantidade_jogos(x)
+
+input(f'{'---'*8}\nPressione qualquer tecla para sair!')
